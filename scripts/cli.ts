9a5cba4649677e7260a3392ba063c05f0e95610b
singlecoder <singlecoder2014@gmail.com>
2025-01-06 16:06:52 +0800

    opt code

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import yargs, { alias } from 'yargs';
+import yargs from 'yargs';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
