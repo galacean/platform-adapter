14e6e68809234476538887088c7255b1fbf3ea7c
singlecoder <singlecoder2014@gmail.com>
2025-04-15 17:21:34 +0800

    opt: opt promise

--- src/platforms/wechat/minigame/engine/ResourceManager.ts
+++ src/platforms/wechat/minigame/engine/ResourceManager.ts
@@ -3,48 +3,40 @@
 /**
  * ResourceManager
  */ var ResourceManager = /*#__PURE__*/ (function () {
-  /**
-   * @internal
-   * @beta Just for internal editor, not recommended for developers.
-   */ _proto.initSubpackages = function initSubpackages(data) {
-    this._virtualPathSubpackageMap = Object.create(null);
-    var _this = this;
-    var subpackages = data.subpacakges;
-    subpackages &&
-      subpackages.forEach(function (subpackage) {
-        _this._virtualPathSubpackageMap[subpackage.root] = subpackage.name;
-      });
-  };
-
-  _proto._loadSubpackage = function _loadSubpackage(itemOrURL) {
-    var _this = this;
-    return new AssetPromise(function (resolve, reject) {
-      var isString = typeof itemOrURL === "string";
-      var testUrl = isString ? itemOrURL : itemOrURL.url;
-
-      var subpackageName = null;
-      if (_this._virtualPathSubpackageMap) {
-        Object.keys(_this._virtualPathSubpackageMap).forEach((key) => {
-          if (testUrl.startsWith(key) || testUrl.startsWith("/public" + key)) {
-            subpackageName = _this._virtualPathSubpackageMap[key];
-            return;
-          }
-        });
-      }
-
-      if (subpackageName) {
+  _proto._loadSubpackageAndMainAsset = function _loadSubpackageAndMainAsset(
+    loader,
+    item,
+    remoteAssetBaseURL,
+    assetBaseURL,
+    subpackageName
+  ) {
+    if (subpackageName) {
+      var _this = this;
+      return new AssetPromise(function (resolve, reject) {
         wx.loadSubpackage({
           name: subpackageName,
           success: () => {
-            resolve();
+            resolve(
+              _this._loadMainAsset(
+                loader,
+                item,
+                remoteAssetBaseURL,
+                assetBaseURL
+              )
+            );
           },
           fail: () => {
             reject(new Error("load subpackage " + subpackageName + " failed"));
           },
         });
-      } else {
-        resolve();
-      }
-    });
+      });
+    } else {
+      return this._loadMainAsset(
+        loader,
+        item,
+        remoteAssetBaseURL,
+        assetBaseURL
+      );
+    }
   };
 })();
