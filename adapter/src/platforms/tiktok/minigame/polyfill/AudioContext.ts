10171f23d0103185871cc312756e7eb590d06852
jtabibito <jtabibito@qq.com>
2025-06-16 15:22:26 +0800

    feat: improve AudioContext features on tiktok

--- adapter/src/platforms/tiktok/minigame/polyfill/AudioContext.ts
+++ adapter/src/platforms/tiktok/minigame/polyfill/AudioContext.ts
@@ -1,17 +1,31 @@
 export default function AudioContext() {
   const audioContext = tt.getAudioContext();
 
-  Object.assign(audioContext, {
-    resume: function (): Promise<void> {
-      return new Promise((resolve, reject) => {
-        audioContext.onStateChange((res) => {
-          if (res.state === 'running') {
-            resolve();
-          }
+  // The following block will assign losing properties to audioContext.
+  const { platform } = tt.getSystemInfoSync();
+  if (platform !== 'devtools') {
+    audioContext.state = 'running';
+    Object.assign(audioContext, {
+      close: function () {
+        audioContext.state = 'closed';
+      },
+      onStateChange: function (callback: (res: { state: string }) => void) {
+        callback({ state: audioContext.state });
+      },
+      resume: function (): Promise<void> {
+        return new Promise((resolve, reject) => {
+          audioContext.onStateChange((res) => {
+            if (res.state === 'running') {
+              resolve();
+            }
+          });
         });
-      });
-    }
-  });
+      },
+      suspend: function () {
+        audioContext.state = 'suspended';
+      },
+    });
+  }
 
   return audioContext;
 }
