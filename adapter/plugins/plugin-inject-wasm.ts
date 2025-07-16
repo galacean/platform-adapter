9739906a5a5210196e689dd1fb67009097f7d9cf
jtabibito <jtabibito@qq.com>
2025-07-16 11:45:00 +0800

    feat: enhance WASM module loading with async and sync handling improvements

--- adapter/plugins/plugin-inject-wasm.ts
+++ adapter/plugins/plugin-inject-wasm.ts
@@ -2,6 +2,7 @@ import { walk } from 'estree-walker';
 import { Plugin } from 'rollup';
 import MagicString from 'magic-string';
 
+// Asynchronous loading and compilation of a WebAssembly (WASM) module.
 function instantiateArrayBuffer(receiver) {
   // @ts-ignore
   return WebAssembly.instantiate(wasmBinaryFile, info)
@@ -13,6 +14,54 @@ function instantiateArrayBuffer(receiver) {
 function instantiateAsync() {
   // @ts-ignore
   return instantiateArrayBuffer(receiveInstantiationResult);
+}
+
+// Synchronously loading and compilation of a WebAssembly (WASM) module.
+function instantiateSync(file, info, receiver) {
+  return WebAssembly.instantiate(file, info).then(function (wasm) {
+    receiver(wasm['instance']);
+  });
+}
+
+function createWasm() {
+  var info = {
+    // @ts-ignore
+    "a": wasmImports
+  };
+  function receiveInstance(instance, module1) {
+    // @ts-ignore
+    wasmExports = instance.exports;
+    // @ts-ignore
+    wasmMemory = wasmExports["J"];
+    // @ts-ignore
+    updateMemoryViews();
+    // @ts-ignore
+    wasmTable = wasmExports["N"];
+    // @ts-ignore
+    addOnInit(wasmExports["K"]);
+    // @ts-ignore
+    removeRunDependency();
+    // @ts-ignore
+    return wasmExports;
+  }
+    // @ts-ignore
+  addRunDependency();
+  // @ts-ignore
+  if (Module["instantiateWasm"]) {
+    try {
+      // @ts-ignore
+      return Module["instantiateWasm"](info, receiveInstance);
+    } catch (e) {
+      // @ts-ignore
+      err(`Module.instantiateWasm callback failed with error: ${e}`);
+      // @ts-ignore
+      readyPromiseReject(e);
+    }
+  }
+  // @ts-ignore
+  // Simulates asynchronous loading and compilation of a WebAssembly (WASM) module.
+  instantiateSync(wasmBinaryFile, info, receiveInstance).catch(readyPromiseReject);
+  return {}; // no exports yet; we'll fill them in later
 }
 
 /**
@@ -30,6 +79,7 @@ export function injectWASM(wasmName: string, modulesInjectWASM?: string[]): Plug
         const magicString = new MagicString(code);
         const ast = this.parse(code);
         let varName: string;
+        let isAsnycCompileResolution = code.includes('instantiateAsync');
         walk(ast, {
           enter(node) {
             if (node.type === 'AssignmentExpression' &&
@@ -44,19 +94,24 @@ export function injectWASM(wasmName: string, modulesInjectWASM?: string[]): Plug
               this.skip();
             }
 
-            if (node.type === 'FunctionDeclaration' &&
-                node.id &&
-                (
-                  node.id.name === 'instantiateAsync' ||
-                  node.id.name === 'instantiateArrayBuffer'
-                )
-            ) {
-              if (node.id.name === 'instantiateAsync') {
-                // @ts-ignore
-                magicString.overwrite(node.start, node.end, instantiateAsync.toString());
-              } else if (node.id.name === 'instantiateArrayBuffer') {
-                // @ts-ignore
-                magicString.overwrite(node.start, node.end, instantiateArrayBuffer.toString());
+            if (node.type === 'FunctionDeclaration' && node.id) {
+              if (isAsnycCompileResolution) {
+                  if (node.id.name === 'instantiateAsync') {
+                    // @ts-ignore
+                    magicString.overwrite(node.start, node.end, instantiateAsync.toString());
+                  } else if (node.id.name === 'instantiateArrayBuffer') {
+                    // @ts-ignore
+                    magicString.overwrite(node.start, node.end, instantiateArrayBuffer.toString());
+                  }
+                }
+              else {
+                if (node.id.name === 'instantiateSync') {
+                  // @ts-ignore
+                  magicString.overwrite(node.start, node.end, instantiateSync.toString());
+                } else if (node.id.name === 'createWasm') {
+                  // @ts-ignore
+                  magicString.overwrite(node.start, node.end, createWasm.toString());
+                }
               }
             }
           }
